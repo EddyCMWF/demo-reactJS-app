4adc743d70c533ac78e72d94cfa209c886cbd409
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2023-11-28 12:07:30 +0000

    component and style

---
+++ src/Components/DataEntryField.jsx
@@ -0,0 +1,16 @@
+// import React from "react";
+
+
+
+const DataEntryField = () => {
+    return (
+        <>
+            Price exc VAT: value
+        </>
+    );
+}
+ 
+export default DataEntryField;
+
+
+
