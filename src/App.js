fe68b3fdc0142a23732629af1f92929a2265bd12
EddyCMWF <Edward.Comyn-Platt@ecmwf.int>
2023-11-29 10:14:09 +0000

    bootstrap cols and rows

--- src/App.js
+++ src/App.js
@@ -1,48 +1,62 @@
 import DataEntryField from './Components/DataEntryField';
 import './App.css';
+import Container from 'react-bootstrap/Container'
 import Header from './Components/Header';
 import Footer from './Components/Footer';
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { DisplayField } from './Components/DisplayField';
+import { VATField } from './Components/VatField';
 
 function App() {
   const [excValue, setExcValue] = useState(0);
   const [incValue, setIncValue] = useState(0);
-
-  const vatRate = 20.0
+  const [vatAmount, setVatAmount] = useState(0);
+  const [vatRate, updateVATRate] = useState(20.0);
 
   const addVat = (price) => {
-    const totalLocalPrice = price * ((vatRate/100.)+1);
+    const LocalPrice = price * ((vatRate/100.)+1);
     setExcValue(price);
-    setIncValue(totalLocalPrice);
+    setIncValue(LocalPrice);
+    updateVatAmount(LocalPrice, price);
     console.log("Adding VAT")
   }
 
   const removeVat = (price) => {
-    const totalLocalPrice = price / ((vatRate/100.)+1);
-    setExcValue(totalLocalPrice);
+    const LocalPrice = price / ((vatRate/100.)+1);
+    setExcValue(LocalPrice);
     setIncValue(price);
+    updateVatAmount(price, LocalPrice);
     console.log("Removing VAT")
   }
 
+  const updateVatAmount = (including, excluding) => {
+    const vatAmountLocal = including - excluding;
+    setVatAmount(vatAmountLocal);
+  }
+
+  useEffect(()=>{
+    addVat(excValue);
+  }, [vatRate])
+
   return (
-    <><div className='App orange-border left-align'>
-      <Header/>
-      <p>Eddy pays tax</p>
-      {/* <p>{DataEntryField({label: 'test', handleValueChanged: setValue, value: {inputValue}})}</p> */}
-      <DataEntryField
-        label='Price excluding VAT' handleValueChanged={addVat}
-        value={excValue === 0 ? "": excValue} 
-      />
-      <DisplayField label="VAT to Pay: " value={vatRate}/>
-      {excValue>=0 ?
-      <DataEntryField
-        label='Price including VAT' handleValueChanged={removeVat}
-        value={incValue === 0 ? "": incValue}
-      />: <></>}
-      <Footer incValue={incValue} />
-    </div></>
-  );
+    <Container className="p-3 bg-dark">
+      <Container className="p-5 mb-4 bg-light rounded-5">
+        <Header/>
+        <VATField vatRateChanged={updateVATRate} currentVATRate={vatRate}/>
+        <DataEntryField
+          label='Price excluding VAT' handleValueChanged={addVat}
+          value={excValue === 0 ? "": excValue} 
+        />
+        <DisplayField label="VAT to Pay: " value={vatAmount}/>
+        {excValue>=0 ?
+        <DataEntryField
+          label='Price including VAT' handleValueChanged={removeVat}
+          value={incValue === 0 ? "": incValue}
+        />: <></>}
+        <Footer incValue={incValue} />
+      </Container>
+    </Container>
+    );
 }
 
 export default App;
---
+++ src/Components/VatField.jsx
@@ -0,0 +1,25 @@
+import { Col, Row } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container'
+
+export const VATField = (props) =>
+{
+    return(
+        <Container className="mb-2">
+            <Row>
+                <Col>
+                    VAT Rate: 
+                </Col>
+                <Col>
+                    <select onChange={(event)=>{
+                            props.vatRateChanged(+event.target.value)
+                        }}> 
+                        <option value="20">20%</option> 
+                        <option value="15">15%</option> 
+                        <option value="12.5">12.5%</option> 
+                        <option value="0">Exempt</option> 
+                    </select>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
